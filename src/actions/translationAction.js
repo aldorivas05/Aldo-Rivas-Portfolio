3da9c0f1dd1667478cc4e2bd1e4c8f5d6b642dee
Aldo Rivas <aldorivas@MacBook-Pro-de-Aldo.local>
2024-03-23 10:33:05 -0400

    Updates

--- src/actions/translationAction.js
+++
@@ -1,4 +0,0 @@
-// actions/translationAction.js
-// actions/translationAction.js
-export const setLanguage = (language) => ({ type: 'SET_LANGUAGE', payload: language });
-export const fetchTranslations = () => ({ type: 'FETCH_TRANSLATIONS' });
